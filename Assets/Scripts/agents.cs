9f15e8358a235de1106872438db3e2ee7a41e7ff
irem <iremaydinn.7@gmail.com>
2022-11-23 11:51:30 +0300

    agents script

--- Assets/Scripts/agents.cs
+++ Assets/Scripts/agents.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class agents : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //component türünde bir obje oluşturman lazım
+    NavMeshAgent agent;
+    public GameObject target;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        agent.SetDestination(target.transform.position);
     }
 }
