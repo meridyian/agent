d40a05c9e778a7e8028d7359c5a314d1ec572e32
irem <iremaydinn.7@gmail.com>
2022-11-23 14:43:37 +0300

    agent script added

--- Assets/Scripts/agents.cs
+++ Assets/Scripts/agents.cs
@@ -8,10 +8,15 @@ public class agents : MonoBehaviour
     //component türünde bir obje oluşturman lazım
     NavMeshAgent agent;
     public GameObject target;
+    public GameObject target2;
+    private string currenttarget;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        currenttarget = "target";
+        agent.SetDestination(target.transform.position);
+        
     }
 
     
@@ -19,4 +24,26 @@ public class agents : MonoBehaviour
     {
         agent.SetDestination(target.transform.position);
     }
+
+    //etkileşime girildiğinde hedef 1 e çarptıysa yeni hedef hedef2 olsun, hedef2 ye çarptıysa 2 olsun
+    //hedeflerde rigidbody olmalı
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(currenttarget=="target")
+        {
+            currenttarget = "target2";
+            agent.SetDestination(target2.transform.position);
+
+        }
+
+
+        else
+        {
+            currenttarget = "target";
+            agent.SetDestination(target.transform.position);
+        }
+
+    }
+
 }
